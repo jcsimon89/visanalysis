e9f10a6f3e5d93a04236836c0894cac20e3bc8b0
jcsimon89 <126107847+jcsimon89@users.noreply.github.com>
2025-02-05 13:01:51 -0800

    Revert "Create visanalysis_bash.bat"
    This reverts commit 04441685785f826e00d7567545b42e2e4cc49792.

--- visanalysis_bash.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-ECHO Hello world
-cd C:\Users\jcsimon\Documents\GitHub\visanalysis
-call activate visanalysis
-python attach_metadata.py --dataset_path 'C:\Users\jcsimon\Documents\Stanford\Data\Bruker\eyesss\JS015_x_JS251\fly_002' --experiment_name 'fly' --rig 'Bruker'
-PAUSE
