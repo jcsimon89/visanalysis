d6f1fb60f13112caed4de69b8d22ebde1747513b
jcsimon89 <126107847+jcsimon89@users.noreply.github.com>
2025-02-05 11:21:38 -0800

    fix bash error and add datagui call

--- visanalysis_bash.bat
+++ visanalysis_bash.bat
@@ -2,5 +2,7 @@
 ECHO Hello world
 cd C:\Users\jcsimon\Documents\GitHub\visanalysis
 call activate visanalysis
-python attach_metadata.py --dataset_path 'C:\Users\jcsimon\Documents\Stanford\Data\Bruker\eyesss\JS015_x_JS251\fly_002' --experiment_name 'fly' --rig 'Bruker'
+python attach_metadata.py --dataset_path C:\Users\jcsimon\Documents\Stanford\Data\Bruker\eyesss\JS015_x_JS251\fly_001 --experiment_name fly --rig Bruker
+cd C:\Users\jcsimon\Documents\GitHub\visanalysis\gui
+python .\DataGUI.py
 PAUSE
