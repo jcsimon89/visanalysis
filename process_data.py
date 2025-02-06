3475154153194778425031d5f2c18448b3ecb024
jcsimon89 <126107847+jcsimon89@users.noreply.github.com>
2025-02-06 13:42:23 -0800

    change to accept experiment_file_directory instead of data_directory (now experiment_file_path)

--- process_data.py
+++ process_data.py
@@ -16,31 +16,34 @@ import matplotlib.pyplot as plt
 from matplotlib.widgets import LassoSelector
 from visanalysis.plugin import base as base_plugin
 
-# call structure: python process_data.py --data_directory "path" --rig "rigID"
+# call structure: python process_data.py --experiment_file_directory "path" --rig "rigID"
 
-# data_directory: string that contains the path to the folder that has the .hdf5 file
+# experiment_file_directory: string that contains the path to the folder that has the .hdf5 file
 # rig: rigID (Bruker or AODscope)
 
-#NOTE: all file names and paths are relative to the data_directory which is the main fly folder ie /fly_001
+#NOTE: all file names and paths are relative to the experiment_file_directory which is the main fly folder ie /fly_001
 
 
 if __name__ == '__main__':
     ## parse shell arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data_directory", nargs="?", help="Folder pointing to hdf5")
+    parser.add_argument("--experiment_file_directory", nargs="?", help="Folder pointing to hdf5")
     parser.add_argument("--rig", nargs="?", help="Bruker or AODscope")
     args = parser.parse_args()
 
-    data_directory = args.data_directory
+    experiment_file_directory = args.experiment_file_directory
     rig = args.rig
 
     # hardcoded file names
     experiment_file_name = 'fly.hdf5'
     json_file_name = 'fly.json'
     
+    # file_path (complete path to .hdf5 file)
+    experiment_file_path = os.path.join(experiment_file_directory, experiment_file_name)
+
     #extract info from fly.json 
 
-    with open(pathlib.Path(data_directory, json_file_name), 'r') as file:
+    with open(pathlib.Path(experiment_file_directory, json_file_name), 'r') as file:
         fly_json = json.load(file)
 
     structural_channel = fly_json['structural_channel']
@@ -50,7 +53,7 @@ if __name__ == '__main__':
 
     #TODO extract subject number from hdf5 or folder name? not sure if needed
 
-    print('experiment_file_name: ' + repr(os.path.join(data_directory, experiment_file_name)))
+    print('experiment_file_name: ' + repr(os.path.join(experiment_file_directory, experiment_file_name)))
 
     if rig == 'Bruker':
         from visanalysis.plugin import bruker
@@ -66,7 +69,7 @@ if __name__ == '__main__':
 
     # attach metadata to hdf5
 
-    plug.attachData(experiment_file_name, data_directory)
+    plug.attachData(experiment_file_name, experiment_file_directory)
 
     print('Attached metadata to {}'.format(experiment_file_name))
 
@@ -75,19 +78,21 @@ if __name__ == '__main__':
     gui_path = str(os.path.join(os.getcwd(),"gui/DataGUI_prog.py"))
 
     os.system('python ' + gui_path
-              + ' --data_directory ' + data_directory
+              + ' --experiment_file_directory ' + experiment_file_directory
               + ' --experiment_file_name ' + experiment_file_name
-              + ' --rig ' + rig)
+              + ' --experiment_file_path ' + experiment_file_path
+              + ' --rig ' + rig
+              + ' --series_number' + series_number)
 
     # series_number = 1
 
-    # plug.updateImagingDataObject(data_directory,
+    # plug.updateImagingDataObject(experiment_file_path,
     #                             experiment_file_name,
     #                             series_number)
 
     
     
-    # plug.updateImageSeries(data_directory,
+    # plug.updateImageSeries(experiment_file_path,
     #                         image_file_name='TSeries-20210707-001_reg.nii',
     #                         series_number=series_number,
     #                         channel=0)
