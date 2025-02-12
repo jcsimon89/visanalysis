b7afa22eb6358ef2f2a4387cf237933ce61e7fcf
jcsimon89 <126107847+jcsimon89@users.noreply.github.com>
2025-02-11 17:23:14 -0800

    Create analyze_data.py

---
+++ analyze_data.py
@@ -0,0 +1,71 @@
+"""
+analyze data:
+1. open hdf5 file
+
+https://github.com/ClandininLab/visanalysis
+mhturner@stanford.edu
+"""
+
+import sys
+import os
+import argparse
+import json
+import pathlib
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.widgets import LassoSelector
+from visanalysis.plugin import base as base_plugin
+from visanalysis.analysis import imaging_data
+
+# call structure: python analyze_data.py --experiment_file_directory "path"
+
+# experiment_file_directory: string that contains the path to the folder that has the .hdf5 file
+
+if __name__ == '__main__':
+    ## parse shell arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--experiment_file_directory", nargs="?", help="Folder pointing to hdf5")
+    args = parser.parse_args()
+
+    experiment_file_directory = args.experiment_file_directory
+
+    # hardcoded file names
+    experiment_file_name = 'fly.hdf5'
+    json_file_name = 'fly.json'
+
+    # extract info from fly.json 
+
+    with open(pathlib.Path(experiment_file_directory, json_file_name), 'r') as file:
+        fly_json = json.load(file)
+
+    struct_channel = [fly_json['structural_channel']] # can only be one structural channel
+    struct_channel_num = [struct_channel[0].split('_')[-1]] #datatype=list of strings
+    print('struct_channel = ' + str(struct_channel))
+    print('struct_channel_num = ' + str(struct_channel_num))
+
+    func_channels = fly_json['functional_channel'].replace("[","").replace("]","").replace("'","").split(",") # can be many functional channels - weird format in snake_brainsss json imports as one big string, cleaning up and converting to list of strings #datatype=list of strings
+    func_channels_num = [func_channels[i].split('_')[-1] for i in range(len(func_channels))] #datatype = list of strings, but individual channel numbers will be converted to int before using methods
+    print('func_channels = ' + str(func_channels))
+    print('func_channel_num = ' + str(func_channels_num))
+
+    #TODO: other data needed from fly_json?
+
+    # file_path (complete path to .hdf5 file)
+    experiment_file_path = os.path.join(experiment_file_directory, experiment_file_name)
+
+    print('experiment_file_name: ' + repr(os.path.join(experiment_file_directory, experiment_file_name)))
+
+    ## start response analysis
+
+    for current_series in series_num: #loop through all series
+        current_series = int(current_series) # methods expect series number to be datatype int
+        #TODO: update imaging object with current series number
+        
+
+        for current_channel in func_channels_num: #loop through channels
+            current_channel = int(current_channel) # methods expect channel number to be datatype int
+            #derive image file name and path
+
+            response_set_name = 'mask_ch' + str(current_channel)
+
+            ID
