ed26d18fd0431e7ded1b524f9d4d85b726d69fa5
jcsimon89 <126107847+jcsimon89@users.noreply.github.com>
2025-02-19 13:27:55 -0800

    Update visanalysis_bash.bat

--- visanalysis_bash.bat
+++ visanalysis_bash.bat
@@ -2,5 +2,9 @@
 ECHO Hello world
 cd C:\Users\jcsimon\Documents\GitHub\visanalysis
 call activate visanalysis
-python process_data.py --experiment_file_directory C:\Users\jcsimon\Documents\Stanford\Data\Bruker\eyesss\JS015_x_JS251\fly_001 --rig Bruker --series_number 3
+python process_data.py --experiment_file_directory C:\Users\jcsimon\Documents\Stanford\Data\Bruker\eyesss\JS139_x_JS251\fly_003 --rig Bruker --series_number 3 --run_gui True --attach_metadata True
+PAUSE
+python analyze_data.py --experiment_file_directory C:\Users\jcsimon\Documents\Stanford\Data\Bruker\eyesss\JS139_x_JS251\fly_003 --rig Bruker --show_figs False --save_figs True
+PAUSE
+select_final_rois.py --experiment_file_directory C:\Users\jcsimon\Documents\Stanford\Data\Bruker\eyesss\JS139_x_JS251\fly_003 --rig Bruker --save True
 PAUSE
