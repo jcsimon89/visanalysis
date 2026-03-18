da820959b8d07af0d4a474c8c4be471bd3acbd33
jcsimon89 <126107847+jcsimon89@users.noreply.github.com>
2026-03-18 10:30:08 -0700

    Update visanalysis_shell.py

--- visanalysis_shell.py
+++ visanalysis_shell.py
@@ -37,7 +37,7 @@ import h5py
 
 # all scripts
 base_path = 'C:/Users/jcsimon/Documents/GitHub/visanalysis'
-experiment_file_directory = 'C:/Users/jcsimon/Documents/Stanford/Data/Bruker/eyesss/JS140_x_JS252/fly_012' #string to folder containing fly.hdf5 file
+experiment_file_directory = 'C:/Users/jcsimon/Documents/Stanford/Data/Bruker/eyesss/JS140_x_JS252/fly_021' #string to folder containing fly.hdf5 file
 rig = 'Bruker' #string "Bruker" or "AODscope"
 
 roi_set_name = 'roi_set_name' # name of roi group to be analyzed (default 'roi_set_name')
@@ -45,8 +45,8 @@ response_set_name = 'mask' # name of response group to be analyzed (default 'mas
 
 # process_data
 series_number_for_roi_selection = '1' #string 
-run_gui = 'False' #string "True" or "False", default = "False"
-attach_metadata = 'False' #string "True" or "False", default = "False"
+run_gui = 'True' #string "True" or "False", default = "False"
+attach_metadata = 'True' #string "True" or "False", default = "False"
 
 # analyze_data
 show_figs = 'False' #string "True" or "False", default = "False"
@@ -105,7 +105,7 @@ os.system('python ' + select_rois_path
 
 
 tag = 'final' #string "raw" or "final"
-dff = 'mean' #string "pre", "mean", or "none", default = "pre"
+dff = 'pre' #string "pre", "mean", or "none", default = "pre"
 
 analyze_data_path = str(os.path.join(base_path,'analyze_data.py'))
 
